83bbc9f83b141883b95fd0fabb34de91e211b37a
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-23 14:24:13 -0500

    pasar data al componente TimeAndLocation y reemplazar valores

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,18 +1,21 @@
+/* eslint-disable react/prop-types */
 
-const TimeAndLocation = () => {
+const TimeAndLocation = ({
+  weather: { formattedLocalTime, name, country },
+}) => {
   return (
     <div>
       <div className="flex items-center justify-center my-6">
-        <p className="text-xl font-extralight">
-          Tuesday, 14 May 2024 | Local time: 07:21 AM
-        </p>
+        <p className="text-xl font-extralight">{formattedLocalTime}</p>
       </div>
 
       <div className="flex items-center justify-center my-3">
-        <p className="text-3xl font-medium">Berlin, DE</p>
+        <p className="text-3xl font-medium">
+          {`${name} ${country}`}
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default TimeAndLocation
