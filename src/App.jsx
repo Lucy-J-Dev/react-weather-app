0558de2ec1876cdea103f10234cb35ab71868153
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-22 23:18:51 -0500

    crear componente para botones superiores

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { FaHeartCircleBolt } from "react-icons/fa6";
+import TopButtons from "./components/TopButtons";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-teal-500">Hello world!</h1>
-      <FaHeartCircleBolt size={80} className="text-red-600"/>
+    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
+      <TopButtons />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
