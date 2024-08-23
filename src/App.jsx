eeafeb9695a9b88a32504fad6d2cd39613d3d7d0
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-22 23:03:32 -0500

    instalar react-icons y probar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { FaHeartCircleBolt } from "react-icons/fa6";
 
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-teal-500">Hello world!</h1>
+      <FaHeartCircleBolt size={80} className="text-red-600"/>
     </div>
   )
 }
